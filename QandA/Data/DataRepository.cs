1c2ae0fac08ab61e6f299c9eb40c660f2bfda541
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-28 22:22:14 +0300

    Implement methods in data repository for data reading

---
+++ QandA/Data/DataRepository.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using QandA.Models;
+
+namespace QandA.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly string _db;
+
+        public DataRepository(IConfiguration db)
+        {
+            _db = db.GetConnectionString("DefaultConnection");
+        }
+
+        public AnswerGetResponse GetAnswer(int answerId)
+        {
+            using (var connection = new SqlConnection(_db))
+            {
+                connection.Open();
+                return connection
+                    .QueryFirstOrDefault<AnswerGetResponse>(@"EXEC dbo.Answer_Get_ByAnswerId @AnswerId = @AnswerId", 
+                    new { AnswerId = answerId});
+            }
+        }
+
+        public QuestionGetSingleResponse GetQuestion(int questionId)
+        {
+            using (var connection = new SqlConnection(_db))
+            {
+                connection.Open();
+                var question = connection.
+                    QueryFirstOrDefault<QuestionGetSingleResponse>(@"EXEC dbo.Question_GetSingle @QuestionId = @QuestionId",
+                    new { QuestionId = questionId });
+                if (question != null)
+                {
+                    question.Answers = connection.Query<AnswerGetResponse>(@"EXEC dbo.Answer_Get_ByQuestionId @QuestionId = @QuestionId",
+                    new { QuestionId = questionId });
+                }
+                return question;
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            using (var connection = new SqlConnection(_db))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(@"EXEC dbo.Question_GetMany");
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search)
+        {
+            using (var connection = new SqlConnection(_db))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(@"EXEC dbo.Question_GetMany_BySearch @Search = @Search", new { Search = search });
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+        {
+            using (var connection = new SqlConnection(_db))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(@"EXEC dbo.Question_GetUnanswered");
+            }
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+            using (var connection = new SqlConnection(_db))
+            {
+                connection.Open();
+                return connection.QueryFirst<bool>(@"EXEC dbo.Question_Exists @QuestionId = @QuestionId", new { QuestionId = questionId});
+            }
+        }
+    }
+}
---
+++ QandA/Data/IDataRepository.cs
@@ -0,0 +1,19 @@
+﻿using QandA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QandA.Data
+{
+    public interface IDataRepository
+    {
+        IEnumerable<QuestionGetManyResponse> GetQuestions();
+        IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
+        IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
+        QuestionGetSingleResponse GetQuestion(int questionId);
+        bool QuestionExists(int questionId);
+        AnswerGetResponse GetAnswer(int answerId);
+
+    }
+}
