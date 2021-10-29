6f56963b040616182cf8d42a7da4f9a8393c91b9
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-29 13:08:16 +0300

    Configure DbUp to do migrations on startup

--- QandA/Startup.cs
+++ QandA/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DbUp;
 
 namespace QandA
 {
@@ -23,9 +24,20 @@ namespace QandA
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges.To.SqlDatabase(connectionString, null)
+                                           .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+                                           .WithTransaction()
+                                           .LogToConsole()
+                                           .Build();
+            if (upgrader.IsUpgradeRequired())
+            {
+                upgrader.PerformUpgrade();
+            }
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -34,7 +46,6 @@ namespace QandA
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
