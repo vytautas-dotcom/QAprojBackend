9045f2c5b04eaf270685a2cb087ef4f60bac679e
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-30 13:31:46 +0300

    Simple validations

--- QandA/Models/QuestionPostRequest.cs
+++ QandA/Models/QuestionPostRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace QandA.Models
 {
     public class QuestionPostRequest
     {
+        [Required(ErrorMessage = "Title is requied")]
+        [StringLength(100)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Content is required")]
         public string Content { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
